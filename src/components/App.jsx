cb13481dc1b74c2a6f1db72e8e4b2243677d9288
mrabushyn <99904851+mrabushyn@users.noreply.github.com>
2022-10-13 20:28:51 +0300

    tasks 1 2 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,15 +14,19 @@ export const App = () => {
   return (
     <div>
       <p className="heders">HW1.1 'Profile'</p>
+
       <Profile user={user} />
 
       <p className="heders">HW1.2 'Statistic'</p>
+
       <Statistics stats={data} />
 
       <p className="heders">HW1.3 'FriendsList'</p>
+
       <FriendsList friends={friends} />
 
       <p className="heders">HW1.4 'Transaction'</p>
+      
       <TransactionHistory items={transactions} />
     </div>
   );
--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -6,10 +6,10 @@ export const Statistics = ({ title, stats }) => {
 // const title = true;
   return (
     <section className={css.statistics}>
-      <h2 className="title">{title ? title : 'Upload stats'}</h2>
+      <h2 className={css.title}>{title ? title : 'Upload stats'}</h2>
       <ul className={css.statList}>
         {stats.map(stat => (
-          <li key={stat.id} className={css.item}>
+          <li className={css.item} key={stat.id} >
             <span className="label">{stat.label} </span>
             <span className="percentage">{stat.percentage}%</span>
           </li>
